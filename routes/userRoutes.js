f6ebfb5a2847809f181729c88c4bb91a6f16682e
Daniel Camaja <dcamajaig@gmail.com>
2025-10-03 11:55:57 -0600

    mejora

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,25 @@
 import express from "express";
-import { listUsers, createUser, updateUser, deleteUser } from "../controllers/usersController.js";
+import {
+  listUsers,
+  createUser,
+  updateUser,
+  deleteUser
+} from "../controllers/usersController.js";
+
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", authenticateToken, listUsers);
-router.post("/", authenticateToken, createUser);
-router.put("/:id", authenticateToken, updateUser);
-router.delete("/:id", authenticateToken, deleteUser);
+// Listar usuarios
+router.get("/list", authenticateToken, listUsers);
+
+// Crear usuario
+router.post("/create", authenticateToken, createUser);
+
+// Actualizar usuario
+router.put("/update/:id", authenticateToken, updateUser);
+
+// Eliminar usuario
+router.delete("/delete/:id", authenticateToken, deleteUser);
 
 export default router;
